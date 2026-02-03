be81acdaa410e1f017bb11d6b9663caad8be0d45
Flavio <flavio.oderzo@etu.univ-amu.fr>
2026-02-03 08:37:40 +0100

    feat: logging in returns roles and id

---
+++ backend/src/Security/LoginSuccessHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): JsonResponse
+    {
+        return new JsonResponse([
+            'message' => 'successfully logged in',
+            'user' => $token->getUser()->getUserIdentifier(),
+            'roles' => $token->getUser()->getRoles(),
+        ], Response::HTTP_OK);
+    }
+}
